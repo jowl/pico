51a246de6959f8d62269adc59a3e816cc2850008
Joel Segerlind <joel@burtcorp.com>
2014-07-17 15:55:24 +0200

    Enable actually moving files
    as well as dry run

--- organizer.go
+++ organizer.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"path"
 	"time"
 )
@@ -16,12 +19,31 @@ type Organizer struct {
 func (o *Organizer) Run() {
 	for picture := range o.Input {
 		dir := o.TimestampPath(picture.Timestamp)
-		base := path.Base(picture.Path)
-		println(path.Join(o.Root, dir, base))
+		fname := path.Base(picture.Path)
+		fullPath := path.Join(o.Root, dir, fname)
+		if o.DryRun {
+			printMove(picture.Path, fullPath)
+		} else {
+			doMove(picture.Path, fullPath)
+		}
 	}
 	o.done <- true
 }
 
 func (o *Organizer) Await() {
-	<- o.done
+	<-o.done
+}
+
+func printMove(source, target string) {
+	fmt.Printf("%v would be moved to %v\n", source, target)
+}
+
+func doMove(source, target string) {
+	var err error
+	if err = os.MkdirAll(path.Dir(target), 0755); err == nil {
+		err = os.Rename(source, target)
+	}
+	if err != nil {
+		log.Printf("Error when moving %v: %v", source, err)
+	}
 }
