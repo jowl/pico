c1f372233a71fde21be4aac62177c5a7847dba2f
Joel Segerlind <joel@burtcorp.com>
2014-07-19 12:09:57 +0200

    Refactor file moving methods in organizer
    to make a dry-run as realistic as possible, without actually doing anything.

--- organizer.go
+++ organizer.go
@@ -23,11 +23,7 @@ func (o Organizer) Run() {
 		}
 		fname := path.Base(picture.Path)
 		fullPath := path.Join(o.root, dir, fname)
-		if o.dryRun {
-			printMove(picture.Path, fullPath)
-		} else {
-			doMove(picture.Path, fullPath)
-		}
+		move(picture.Path, fullPath, o.dryRun)
 	}
 	o.done <- true
 }
@@ -36,22 +32,24 @@ func (o Organizer) Await() {
 	<-o.done
 }
 
-func printMove(source, target string) {
-	fmt.Printf("%v would be moved to %v\n", source, target)
-}
-
-func doMove(source, target string) {
+func move(source, target string, dryRun bool) {
 	var err error
-	if err = os.MkdirAll(path.Dir(target), 0755); err == nil {
-		if _, err = os.Stat(target); os.IsNotExist(err) {
-			err = os.Rename(source, target)
+	if _, statErr := os.Stat(target); os.IsNotExist(statErr) {
+		if !dryRun {
+			if err = os.MkdirAll(path.Dir(target), 0755); err == nil {
+				if err = os.Rename(source, target); err == nil {
+					LogInfof("Moved %v to %v", source, target)
+				}
+			}
 		} else {
-			err = os.ErrExist
+			fmt.Printf("%v would be moved to %v\n", source, target)
 		}
+	} else {
+		err = os.ErrExist
 	}
+
+
 	if err != nil {
 		LogErrorf("Couldn't move %v: %v", source, err)
-	} else {
-		LogInfof("Moved %v to %v", source, target)
 	}
 }
